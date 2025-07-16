55cf5d55ad018638f1bc06dd29d243f54f564793
Preet Rana <preetrana@Preets-MacBook-Air.local>
2025-07-16 10:46:17 +0530

    start

---
+++ includes/textEditor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Preet Rana on 16/07/25.
+//
+
+#include "textEditor.h"
---
+++ includes/textEditor.h
@@ -0,0 +1,16 @@
+//
+// Created by Preet Rana on 16/07/25.
+//
+
+#ifndef TEXTEDITOR_H
+#define TEXTEDITOR_H
+
+
+
+class textEditor {
+
+};
+
+
+
+#endif //TEXTEDITOR_H
